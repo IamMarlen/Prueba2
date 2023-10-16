32e13bfd73d62dc6a6818f66c91eb73dd90236f9
Marlen <elizamendoza1205@gmail.com>
2023-10-16 20:42:45 -0300

    prueba2

---
+++ script.js
@@ -0,0 +1,12 @@
+let presupuesto = 100;
+presupuesto = 120;
+
+let gastos 
+
+console.log("Mis gastos son:")
+
+let papol = undefined;
+
+console.log(papol);
+
+let papel = 20.9; //los decimales en js se escriben con puntos 
